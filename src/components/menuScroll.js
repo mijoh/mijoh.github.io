2d5ca1df45af503f5bd6ffb4d63198674e22e09f
Tim Jackleus <timjackleus@Tims-MacBook-Pro-2.local>
2020-11-01 13:44:56 +0100

    chore: change webpack to parcel

--- src/components/menuScroll.js
+++ src/components/menuScroll.js
@@ -1,16 +1,15 @@
-require('smoothscroll-polyfill').polyfill()
+// require('smoothscroll-polyfill').polyfill()
 
 const menuScroll = (scrollPositions, scrollTriggers) => {
-  for(let i = 0; i < scrollTriggers.length; i ++) {
-
-    scrollTriggers[i].addEventListener('click', () => {
+  for (let i = 0; i < scrollTriggers.length; i++) {
+    scrollTriggers[i].addEventListener("click", () => {
       window.scroll({
-        'left': 0,
-        'top': scrollPositions[i] || scrollPositions,
-        'behavior': 'smooth'
-      })
-    })
+        left: 0,
+        top: scrollPositions[i] || scrollPositions,
+        behavior: "smooth",
+      });
+    });
   }
-}
+};
 
-export default menuScroll
+export default menuScroll;
---
+++ src/scripts.js
@@ -0,0 +1,25 @@
+import menuHighlight from "./components/menuHighlight";
+import menuScroll from "./components/menuScroll";
+
+const scrollPositionElements = Array.from(
+  document.querySelectorAll(".js-scroll-pos")
+);
+const scrollTriggers = Array.from(document.querySelectorAll(".js-scroll-nav"));
+
+// Get margin of scroll position Elements
+const scrollPositionMargin = parseInt(
+  window.getComputedStyle(scrollPositionElements[0]).marginTop
+);
+
+// Create array of scroll positions + element margin
+const scrollPositionsWithMargins = scrollPositionElements.map(
+  (sp) => sp.offsetTop - scrollPositionMargin
+);
+const scrollPositions = scrollPositionElements.map((sp) => sp.offsetTop);
+
+menuHighlight(scrollPositionsWithMargins, scrollTriggers);
+menuScroll(scrollPositions, scrollTriggers);
+
+// Scroll to last stop on heading click
+const heading = Array.from(document.querySelectorAll(".heading-desktop"));
+menuScroll(scrollPositions.slice(-1)[0], heading);
