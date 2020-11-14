ebb36a70fa86e81eddb6c1fbaff77f74b66384d1
timjackleus <timjackleus@gmail.com>
2020-11-14 22:03:46 +0100

    chore: update build script

--- build.sh
+++ build.sh
@@ -2,4 +2,8 @@
 
 rm index.html
 cp dist/index.html .
-sed -e 's/src=\"/src=\"\/dist/g' -e 's/href=\"/href=\"\/dist/g' index.html
+sed -i '' \
+  -e 's/src=\"/src=\"dist/g' \
+  -e 's/href=\"/href=\"dist/g' \
+  -e 's/href=\"disthttps/href=\"https/g' \
+  -e 's/href=\"distmail/href=\"mail/g' index.html
